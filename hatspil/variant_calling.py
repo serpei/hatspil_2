fca37e7d623d3dabc4d4a35d7b80521ab5f6b288
serpei <44779200+serpei@users.noreply.github.com>
2024-11-16 09:39:11 +0100

    Update variant_calling.py

--- hatspil/variant_calling.py
+++ hatspil/variant_calling.py
@@ -615,7 +615,7 @@ class VariantCalling:
             panel_drug.gene_symbol
         )
 
-        self.variants = pd.read_csv(self.variants_filename, index_col=False, on_bad_lines='warn')
+        self.variants = pd.read_csv(self.variants_filename, index_col=False, on_bad_lines='skip')
         annotation.reset_index(inplace=True, drop=True)
         self.variants.reset_index(inplace=True, drop=True)
 
@@ -690,7 +690,7 @@ class VariantCalling:
         if config.use_mongodb:
             from pymongo.errors import DocumentTooLarge
 
-            self.variants = pd.read_csv(self.variants_filename, index_col=False, on_bad_lines='warn')
+            self.variants = pd.read_csv(self.variants_filename, index_col=False, on_bad_lines='skip')
             variants = [
                 {
                     key.replace(".", " "): value
